69d632883d69f7b43488b46e77dbf2f0df8c1623
Lord Corgo <Business@Corgo.org>
2023-10-05 17:00:31 -0600

    Corrected vite relative

--- ZeroZone/vite.config.js
+++ ZeroZone/vite.config.js
@@ -4,5 +4,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: './', // Set the base URL to a relative path
 });
 
